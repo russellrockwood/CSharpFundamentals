7bb67342301d19f58ea4815b133e770944e99cba
Russell Rockwood <russell.rockwood@gmail.com>
2021-05-06 15:30:17 -0400

    added currency interface

---
+++ 08_Interfaces/Currency/CurrencyTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _08_Interfaces.Currency
+{
+    [TestClass]
+    public class CurrencyTests
+    {
+        [TestMethod]
+        public void CurrencyValueTests()
+        {
+            ICurrency penny = new Penny();
+            ICurrency dime = new Dime();
+            ICurrency dollar = new Dollar();
+
+            Assert.AreEqual(.01m, penny.Value);
+            Assert.AreEqual(.10m, dime.Value);
+            Assert.AreEqual(1m, dollar.Value);
+        }
+
+        [DataTestMethod]
+        [DataRow(100.2)]
+        [DataRow(37.12)]
+        [DataRow(1.32)]
+        [DataRow(18)]
+
+        public void EPaymentTest(double value)
+        {
+            decimal convertedValue = Convert.ToDecimal(value);
+
+            var ePay = new ElectronicPayment(convertedValue);
+
+            Assert.AreEqual(convertedValue, ePay.Value);
+
+        }
+    }
+}
--- 08_Interfaces/IFruitTests.cs
+++ 08_Interfaces/IFruitTests.cs
@@ -1,6 +1,7 @@
 ﻿using _08_Interfaces.Fruit;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace _08_Interfaces
 {
@@ -17,6 +18,103 @@ namespace _08_Interfaces
 
             var output = banana.Peel();
             Console.WriteLine(output);
+
+            Console.WriteLine("The banana is peeled: " + banana.IsPeeled);
+            Assert.IsTrue(banana.IsPeeled);
+        }
+
+        [TestMethod]
+        public void InterfacesInCollections()
+        {
+            Orange orange = new Orange();
+
+            var fruitSalad = new List<IFruit>
+            {
+                new Banana(),
+                new Orange(),
+                orange
+            };
+
+            // Orange exclusive methods still accessible outside of IFruit collection
+            orange.Squeeze();
+
+            foreach (var fruit in fruitSalad)
+            {
+                Console.WriteLine(fruit.Name);
+                Console.WriteLine(fruit.Peel());
+                // No longer accessible once in a collection
+                // fruit.Squeeze
+
+                Assert.IsInstanceOfType(orange, typeof(IFruit));
+            }
+
+            Assert.IsInstanceOfType(orange, typeof(Orange));
+
+        }
+
+        private string GetFruitName(IFruit fruit)
+        {
+            return $"This fruit is called {fruit.Name}";
+        }
+
+        [TestMethod]
+        public void InterfacesInMethods()
+        {
+            var grape = new Grape();
+
+            // Even though the method only takes IFruit, grape still qualifies
+            string output = GetFruitName(grape);
+
+            Console.WriteLine(output);
+        }
+
+        [TestMethod]
+        public void TypeOfInstance()
+        {
+            var fruitSalad = new List<IFruit>
+            {
+                new Orange(true),
+                new Orange(true),
+                new Grape(),
+                new Orange(),
+                new Banana(true),
+                new Grape(),
+            };
+
+            Console.WriteLine("Is the orange peeled?");
+            foreach (var fruit in fruitSalad)
+            {
+                // Checking it's of type orange, casting it as orange
+                // Pattern Matching
+                if (fruit is Orange orange)
+                {
+                    if (orange.IsPeeled)
+                    {
+                        Console.WriteLine("It's a peeled orange");
+                        // Regain orange exclusive properties
+                        orange.Squeeze();
+                    }
+                    else
+                    {
+                        Console.WriteLine("It is an orange.");
+                    }
+                }
+                else if (fruit.GetType() == typeof(Grape))
+                {
+                    Console.WriteLine("It's a grape");
+                    // Without pattern matching, cast is necessary
+                    var grape = (Grape)fruit;
+                    Console.WriteLine(grape.Peel());
+                }
+                else if (fruit.IsPeeled)
+                {
+                    Console.WriteLine("It's a peeled banana");
+                }
+                else
+                {
+                    Console.WriteLine("it's a banana");
+                }
+            }
         }
     }
 }
